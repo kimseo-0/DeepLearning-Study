8adc3933d261ac9354c494f39c46e4653817e53b
sy <42965120+kimseo-0@users.noreply.github.com>
2025-08-17 11:56:59 +0900

    feat: 이미지 분류 모델 추가

---
+++ src/tools/image_util.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+import torch
+
+def denormalize(image, mean, std):
+    mean = torch.tensor(mean).view(3, 1, 1)
+    std = torch.tensor(std).view(3, 1, 1)
+    return image * std + mean
+
+def view_img(tensor_image, title = "title"):
+    img_data = tensor_image.numpy()
+
+    # 이미지를 시각화 해보자
+    plt.imshow(np.transpose(img_data, (1, 2, 0)))
+    plt.title(title)
+    plt.show()
+print("안녕")
